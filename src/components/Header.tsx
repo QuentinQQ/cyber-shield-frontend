ceaba8c4870d6c1169d27d847785890449f7c459
QuentinQ <quentin.zt.qian@gmail.com>
2025-04-11 13:23:23 +1000

    Fix error import

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Menu, X, Home, Users, BookOpen, Headphones, Shield, Heart } from "lucide-react";
+// import { Menu, X, Home, Users, BookOpen, Headphones, Shield, Heart } from "lucide-react";
+import { Menu, X, Home, Users, BookOpen, Shield, Heart } from "lucide-react";
 import { Link, useNavigate } from 'react-router-dom';
 import PrimaryButton from './PrimaryButton';
 import logo from '@/assets/logo.svg';
