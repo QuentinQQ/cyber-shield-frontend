02316614f754f33776916da965fdb10b39c92455
QuentinQ <quentin.zt.qian@gmail.com>
2025-05-18 21:25:03 +1000

    Fixed build error

--- src/pages/Video.tsx
+++ src/pages/Video.tsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import YouTube from 'react-youtube';
-import PrimaryButton from "../components/PrimaryButton";
-import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { TeleportBubble } from "@/components/TeleportBubble";
 
